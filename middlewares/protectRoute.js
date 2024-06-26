b0c9e6563e0cb475b5a8aeae637e8b7d1887ae4c
vyshnavkv <vyshnavkv89@gmail.com>
2024-06-26 09:46:03 +0530

    middleware issue solved

--- middlewares/protectRoute.js
+++ middlewares/protectRoute.js
@@ -5,8 +5,8 @@ configDotenv()
 
 const protectRoute = async (req, res, next) => {
 	try {
-		const token = req.cookies.jwt;
-		console.log(token);
+		const token = req.cookies.my_custom_jwt;
+		console.log(req.cookies);
 
 		if (!token) {
 			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
